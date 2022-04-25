0b388d20abab3484692d15392c608d34a1787ba4
abbas_alkaabi <abbasalkabbi2003@gmail.com>
2022-04-25 21:28:26 +0300

    add style to page 404

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,25 @@
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+      <div class="container mt-5">
+        <div class="row d-flex justify-content-center">
+                 <div className="col-md-7">
+                           <divl cassName="card p-3 py-4 text-center">
+                               <h1 className="h1 text-center">
+                                   Oops!</h1>
+                               <h2 className="text-center mt-2">
+                                   404 Not Found</h2>
+                               <div class="error-details text-center mt-4">
+                                   Sorry, an error has occured, Requested page not found!
+                               </div>
+                               <div class="error-actions text-center mt-5">
+                                   <a href="/" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                                       Take Me Home </a><a href="/" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Support </a>
+                               </div>
+                           </divl>
+                  </div>
+         </div>
+      </div>
+    )
   };
   
   export default NoPage;
